cba3a433427f2913d09f78bb55ec5cc424ea06de
= <=>
2015-04-03 00:43:18 +0300

    first push

---
+++ P3-DatabaseDeneme/app/src/main/java/com/example/macbookpro/p3_databasedeneme/Kayitlar.java
@@ -0,0 +1,10 @@
+package com.example.macbookpro.p3_databasedeneme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kayitlar {
+
+    public static List<Oyuncular> oyuncular = new ArrayList<Oyuncular>();
+    public static DatabaseHelper dbHelper;
+}
